d8075f58d019bd874d680883c6e6eb340e638f80
SalmY001 <80605132+SalmY001@users.noreply.github.com>
2024-04-03 21:56:17 +0100

    clear screen & remove decimals in app.js

--- weather-be-good-app/src/App.js
+++ weather-be-good-app/src/App.js
@@ -37,15 +37,17 @@ const searchLocation = (event) => {
             {data.sys ? <p>{data.sys.country}</p> : null}
           </div>
           <div className="temp">
-            {data.main ? <h1>{data.main.temp}°C</h1> : null}
+            {data.main ? <h1>{data.main.temp.toFixed()}°C</h1> : null}
           </div>
           <div className="description">
             {data.weather ? <p>{data.weather[0].main}</p> : null}
           </div>
         </div>
+
+        {data.name !== undefined && // Clear screen
         <div className="bottom">
           <div className="feels_like">
-           {data.main ? <p className='bold'>{data.main.feels_like}°C</p> : null}
+           {data.main ? <p className='bold'>{data.main.feels_like.toFixed()}°C</p> : null}
             <p>Feels Like</p>
           </div>
           <div className="humidity">
@@ -53,17 +55,12 @@ const searchLocation = (event) => {
             <p>Humidity</p>
           </div>
           <div className="wind">
-            {data.wind ? <p className='bold'>{data.wind.speed}MPH</p> : null}
+            {data.wind ? <p className='bold'>{data.wind.speed.toFixed()}MPH</p> : null}
             <p>Wind Speed</p>
           </div>
-
         </div>
-
-        
-      </div> 
-
-      
-    
+        }
+      </div>
     </div>
   );
 }
